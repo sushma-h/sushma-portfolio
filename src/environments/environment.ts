ba0e15bb826301137244df185e5853bb8470d15e
sushma-h <sushma111297@gmail.com>
2021-11-10 12:13:43 +0530

    myphoto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDnDOjLsM-MGFCXYGGqFfK1lvSmXmzbsMs',
-    authDomain: 'myportfolio-35e7b.firebaseapp.com',
-    databaseURL: 'https://myportfolio-35e7b-default-rtdb.firebaseio.com',
-    projectId: 'myportfolio-35e7b',
-    storageBucket: 'myportfolio-35e7b.appspot.com',
-    messagingSenderId: '35573057468',
-    appId: '1:35573057468:web:14b71a247eb5aa881f0db9',
+    apiKey: "AIzaSyDnDOjLsM-MGFCXYGGqFfK1lvSmXmzbsMs",
+    authDomain: "myportfolio-35e7b.firebaseapp.com",
+    databaseURL: "https://myportfolio-35e7b-default-rtdb.firebaseio.com",
+    projectId: "myportfolio-35e7b",
+    storageBucket: "myportfolio-35e7b.appspot.com",
+    messagingSenderId: "35573057468",
+    appId: "1:35573057468:web:14b71a247eb5aa881f0db9",
   },
 };
 
